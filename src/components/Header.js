62060a81066643a7088fb5be8f3aa51e457f0658
erico0301 <76524313+erico0301@users.noreply.github.com>
2022-03-22 20:34:01 +0700

    Add

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Header component</h1>
+            </div>
+        )
+    }
+}export default Header;
